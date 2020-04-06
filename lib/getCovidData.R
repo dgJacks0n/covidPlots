67f25e6a8cccfaf79b45eb737256b80e91dd035f
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-04-05 22:24:07 -0400

    Cache data to file, only reload if over 12h old.  Calculate days from first when data is loaded.

--- lib/getCovidData.R
+++ lib/getCovidData.R
@@ -2,12 +2,18 @@ suppressPackageStartupMessages({
 	library(httr)
 	library(dplyr)
 	library(lubridate)
+	library(here)
 	library(jsonlite)
 })
 
+#' getDataFilePath: path for data file
+getDataFilePath <- function() {
+	paste(here::here(), "covid19data.Rds", sep = "/")
+}
 
 #' downloadCovidData: get data from statworx api
-downloadCovidData <- function(covidUrl = "https://api.statworx.com/covid") {
+downloadCovidData <- function(covidUrl = "https://api.statworx.com/covid",
+															verbose = T) {
 # Post to API
 payload <- list(code = "ALL")
 response <- httr::POST(url = covidUrl,
@@ -21,21 +27,61 @@ df <- data.frame(fromJSON(content),
 # convert date to date class object
 df$date <- as.Date(df$date)
 
+# calculate relative days from first case
+rel_date <- df %>% 
+	filter(cases_cum > 0) %>% 
+	group_by(code) %>% 
+	mutate(d_from_first_case = as.numeric(date - min(date))) %>%
+	ungroup() %>%
+	select(date, country, d_from_first_case)
+
+# add to data frame
+df <- left_join(df, rel_date, by = c("date", "country"))
+
+
 # tag with URL and date
 attr(df, "url") <- covidUrl
 
 attr(df, "downloaded") <- Sys.time()
 
 
+# save as RDS file
+datafile <- getDataFilePath()
 
-return(df)
+saveRDS(df, file = datafile)
+
+if(verbose) {
+	message("Retrieved covid19 data from ", covidUrl, 
+			" and saved it to ", datafile)
+}
+
+return(TRUE)
+
 }
 
 #' getCovidData: retrieve covid dataset from memory or download
-#' note- add arg for data age
-getCovidData <- function(...) {
-	if(!exists("covidData")) {
-		covidData <- downloadCovidData(...)
+#' staleness: data age in hours
+getCovidData <- function(staleness = 12) {
+	# if loaded: feturn
+	if(exists("covidData")) {
+		return(covidData)
 	}
+	
+	datafile <- getDataFilePath()
+	
+	# if cached file doesn't exist or is too old, download
+	if(!file.exists(datafile)) {
+		downloadCovidData()
+	} else {
+		dfage <- as.numeric(now() - file.mtime(datafile),
+												units = "hours")
+		
+		if(dfage >= staleness) {
+			downloadCovidData()
+		}
+	}
+	
+	covidData <- readRDS(datafile)
+	
 	return(covidData)
 }
