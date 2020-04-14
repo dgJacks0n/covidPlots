4758be9e922ce7993a0a79a36a927fdc1d9cf19a
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-04-14 14:50:47 -0400

    Split off code to download US map data for chloropleth; serialize to file to avoid repeated downloads

---
+++ code/getUsMap.R
@@ -0,0 +1,49 @@
+#!Rscript
+
+# getUsMap.R
+# download Mapbox file for US counties to support US chlorpleth maps
+library(rjson)
+library(optparse)
+
+
+# define command line options
+option_list <- list(
+	make_option(c("-o", "--outputfile"),
+							type = "character",
+							help = "Path to output RDS file",
+							action = "store"),
+	make_option(c("-v", "--verbose"),
+							action = "store_true",
+							default = FALSE,
+							help = "Show verbose output (default: f)",
+	)
+)
+
+opts <- parse_args(OptionParser(option_list = option_list))
+	
+outFile <- opts$outputfile
+verbose <- opts$verbose
+
+if(verbose) { message("Writing US map to ", outFile) }
+
+
+
+# load county map
+countyMapUrl = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
+
+countyMapData<- rjson::fromJSON(file=countyMapUrl)
+
+# tag with info on date and time of download 
+attr(countyMapData, "source") <- countyMapUrl
+
+attr(countyMapData, "downloaded") <- Sys.time
+
+# write output file
+saveRDS(countyMapData, file = outFile)
+
+if(verbose) {
+	message("Downloaded US map from ", countyMapUrl,
+					" at ", attr(countyMapData, "downloaded"),
+					" and serialized to ", outFile)
+}
+
