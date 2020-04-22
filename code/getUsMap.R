6958b1f7469999cce531f5f0615f1cad1b3e2d51
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-04-22 14:55:15 -0400

    Converted from stand-alone script to function

--- code/getUsMap.R
+++ code/getUsMap.R
@@ -1,49 +1,22 @@
-#!Rscript
+#' Download Mapbox file for US counties to support US chlorpleth maps
+#' 
+#' @param mapUrl URL for US county map indexed by FIPS codes
+#' 
+#' @return mapData annotated with attributes 'source' (mapUrl) and 'timestamp' (download time)
+#' 
+#' @export
 
-# getUsMap.R
-# download Mapbox file for US counties to support US chlorpleth maps
-library(rjson)
-library(optparse)
+getUsMap <- function(mapUrl = 
+										 	'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') {
 
-
-# define command line options
-option_list <- list(
-	make_option(c("-o", "--outputfile"),
-							type = "character",
-							help = "Path to output RDS file",
-							action = "store"),
-	make_option(c("-v", "--verbose"),
-							action = "store_true",
-							default = FALSE,
-							help = "Show verbose output (default: f)",
-	)
-)
-
-opts <- parse_args(OptionParser(option_list = option_list))
+	countyMapData<- rjson::fromJSON(file = mapUrl)
 	
-outFile <- opts$outputfile
-verbose <- opts$verbose
-
-if(verbose) { message("Writing US map to ", outFile) }
-
-
-
-# load county map
-countyMapUrl = 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
-
-countyMapData<- rjson::fromJSON(file=countyMapUrl)
-
-# tag with info on date and time of download 
-attr(countyMapData, "source") <- countyMapUrl
-
-attr(countyMapData, "downloaded") <- Sys.time()
-
-# write output file
-saveRDS(countyMapData, file = outFile)
-
-if(verbose) {
-	message("Downloaded US map from ", countyMapUrl,
-					" at ", attr(countyMapData, "downloaded"),
-					" and serialized to ", outFile)
+	# tag with info on date and time of download 
+	attr(countyMapData, "source") <- mapUrl
+	
+	attr(countyMapData, "timestamp") <- Sys.time()
+	
+	return(countyMapData)
 }
+
 
