89e823cf3286378ff645e472b7a14135d66c7b8e
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-04-23 10:40:20 -0400

    Import and re-export pipe operator from magrittr

---
+++ R/imports.R
@@ -0,0 +1,11 @@
+#' imports.R: import functions from other packages
+#' 
+#' #' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
