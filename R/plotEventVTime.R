8568b62278af906b7e030c5201f9bca1c8df0b8a
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-04-27 09:57:44 -0400

    Moved addRelativeDate function from R/plotEventTime.R to  R/addRelativeDate.R

--- R/plotEventVTime.R
+++ R/plotEventVTime.R
@@ -77,27 +77,3 @@ plotEventVTime <- function(pData, level = c("state", "county"),
 	return(iPlot)
 }
 
-# relativeDate: calculate days relative to first case by group.
-addRelativeDate <- function(myData, group = c("state", "county")) {
-	# use fips to group by county
-	groupCol <- ifelse(match.arg(group) == "state", "state", "fips")
-	
-	# get first case for each group
-	firstCase <- myData %>% 
-		dplyr::filter(cases > 0) %>%
-		dplyr::group_by_at(groupCol) %>%
-		dplyr::summarise(first_case_date = min(date)) %>%
-		dplyr::ungroup()
-	
-	# add to myData
-	myData <- dplyr::left_join(myData, firstCase, by = groupCol)
-	
-	# calculate days from first case
-	myData$days_from_first_case <- 
-		as.integer(myData$date - myData$first_case_date, unit = "days")
-	
-	myData$first_case_date <- NULL
-	
-	return(myData)
-	
-}
