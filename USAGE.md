0ea31710c8071776aac9f29fde8769fd04ddf936
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-04-28 11:52:03 -0400

    make.R: added command line options, steps for 'clean' and 'update', fixed dynamic output file, make sure population data is recognized as a dependency USAGE.md: updated to reference command-line help in make.R

--- USAGE.md
+++ USAGE.md
@@ -1,11 +1,10 @@
 ## Usage
 
-Generation of these analyses is controlled by `make` using the
-[Makefile](./Makefile) in this directory.  
+Generation of these analyses is controlled by `drake` using the master script
+[make.R](./make.R) in this directory.  Run
+```
+Rscript make.R -h
+```
+for options and information
 
-From a shell in this directory:
 
-+ To update the analysis, run `make all`
-+ To remove all current analyses and regenrate all outputs, run `make clean; make all`
-
-See the [Makefile](./Makefile) for additional options.
--- make.R
+++ make.R
@@ -1,19 +1,39 @@
 # make.R
 # master script for use with drake package
-
+# run 'Rscript make.R -h' for usage.
 
 # packages
+library(optparse)
+
+optList <- list(
+	make_option(c("-o", "--outdir"), action = "store", 
+							default = "./results",
+							help = "Directory for knitted output"),
+	make_option(c("-f", "--format"), action = "store", default = "html",
+							help = "Format for knitted results - currently only HTML is supported"),
+	make_option(c("-u", "--update"), action = "store_true", default = F,
+							help = "Updae case counts only"),
+	make_option(c("-c", "--clean"), action = "store_true", default = F,
+							help = "Remove previously downloaded inputs and results")
+)
+
+opt <- parse_args(OptionParser(option_list = optList))
+
+
+
+# load additional packages
 library(drake)
 library(here)
 library(covidPlots)
+library(optparse)
+library(dplyr)
 
-# where to write reports and what format
-resultDir <- paste(here(), "results", sep = "/")
-
-resultType = "html"
-
-# generate output file based on input
+# functions
+# resultFile: generate output file based on input
 resultFile <- function(rmdFile, resDir, resType) {
+	# convert directory to relative path
+	resDir <- normalizePath(resDir, winslash = "/")
+	
 	newbase <- sub("\\.[A-Za-z]+$", "", basename(rmdFile))
 	
 	newbase <- paste(newbase, resType, sep = ".")
@@ -23,21 +43,33 @@ resultFile <- function(rmdFile, resDir, resType) {
 	return(resPath)
 }
 
+
+# clean or update
+if(opt$clean) {
+	# clean removes all cached files
+	clean()
+} else {
+	if(opt$update) {
+		clean(list = c("stateData", "countyData"))
+	}
+}
+
 # define analysis workflow
 plan <- drake_plan(
-	stateData  = getUsStateCases(),
+	stateData  = getUsStateCases(popData = readd(statePopulations)),
 									 
-	countyData = getUsCountyCases(),
-
+	countyData = getUsCountyCases(popData = readd(countyPopulations)),
+	
 	countyPopulations = getUsCountyPopulation(),
 	
 	statePopulations = getUsStatePopulation(),
 	
 	usMap = getUsMap(),
-	
+
 	rmarkdown::render(
 		knitr_in("./us_covid_rates.Rmd"),
-		output_file = file_out("./us_covid_rates.html"),
+		output_file = file_out(!! resultFile("us_covid_rates.Rmd", 
+																				 opt$outdir, opt$format)),
 		envir = new.env(),
 		quiet = TRUE
 	)
