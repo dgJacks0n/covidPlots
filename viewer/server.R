6d90e12458d79209c44ea71a6a46300bd4badafe
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-05-01 17:05:39 -0400

    viewer/server.R, viewer/ui.R: initial content added for event plots (current and time) and for event map.  Map is very slow.

--- viewer/server.R
+++ viewer/server.R
@@ -1,20 +1,77 @@
+# Viewer for covid 19 data
 #
-#
-
+ 
 library(shiny)
+library(drake)
+library(plotly)
+library(covidPlots) # should be installed from ../lib/covidPlots
+library(ggToys)
+
+# load data
+loadd(stateData)
+loadd(countyData)
+loadd(usMap)
+
+lastStateData <- getLastStateData(stateData)
+
+lastCountyData <- getLastCountyData(countyData)
+
+ggplot2::theme_set(theme_dj())
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    eventChoice <- reactive({ input$event })
+    
+    selectedStates <- reactive({ 
+      sPoints <- event_data("plotly_click")
+      
+      return(sPoints$key)
     })
+    
+    output$eventPlot <- renderPlotly({
+        
+        p_casesByState <- plotCurrentCount(lastStateData, xCol = "state",
+                                           value = eventChoice(),
+                                           hGroupName = "byState")
+        
+        p_caseGrowthByState <- plotEventVTime(stateData, 
+                                              value = eventChoice(),
+                                              hGroupName = "byState")
 
+       comboPlot <-  plotly::subplot(p_casesByState, p_caseGrowthByState,
+                        widths = c(0.25, 0.75),
+                        shareX = F, shareY = T,
+                        which_layout = 1) %>%
+            plotly::highlight(dynamic = F, 
+                              selectize = T, 
+                              color = c("red", "green", "blue"),
+                              persistent = T,
+                              on = "plotly_click")
+                          
+            
+      
+    
+    })
+    
+    output$eventMap <- renderPlotly({
+      # works but horribly slow...
+      eventMap <- plotUsMap(lastCountyData, event = eventChoice(), map = usMap)
+    })
+    
+    output$info <- renderPrint({
+      event_data("plotly_click")
+    })
+    
+    output$dataAnnotation <- renderText({
+      paste("Data was downloaded from",
+            attr(stateData, "source"),
+            "at", attr(stateData, "timestamp"),
+            "and includes cases through",
+            max(stateData$date))
+    })
+    
+    output$selectedStates <- renderText({
+      req(selectedStates())
+      paste("Selected States:", selectedStates())
+    })
 })
--- viewer/ui.R
+++ viewer/ui.R
@@ -1,6 +1,7 @@
 # Shiny Viewer for US Covid data
 
 library(shiny)
+library(plotly)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -11,16 +12,24 @@ shinyUI(fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput("event", "Event to plot",
+                        choices = c("Cases" = "cases",
+                                    "Cases per Capita" = "cases_per_capita",
+                                    "Deaths" = "deaths",
+                                    "Deaths per Capita" = "deaths_per_capita"),
+                        multiple = F),
+            textOutput("dataAnnotation")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            tabsetPanel(
+                tabPanel("Events", plotlyOutput("eventPlot"),
+                         textOutput("selectedStates")
+                         #verbatimTextOutput("info")
+                         ),
+                tabPanel("Map", plotlyOutput("eventMap"))
+            )
         )
     )
 ))
