d77d7cb5a4194250926284ca6bc9c5548a3a0b54
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-11-30 21:59:56 -0500

    lib/covidPlots/R/plotDoublingTime.R: changed to plot growth rate instead of doubling time

--- lib/covidPlots/R/plotDoublingTime.R
+++ lib/covidPlots/R/plotDoublingTime.R
@@ -1,4 +1,4 @@
-#' Plot windowed doubling time in days for cases or deaths by state
+#' Plot windowed growth rate in days for cases or deaths by state
 #' growth rates are calculated by performing a lm fit on log10(event count) over the window. 
 #' Only state level data is supported - counties may not have enough cases
 #' 
@@ -10,7 +10,7 @@ plotDoublingTime <- function(eData = getUsStateCases(), event = c("cases", "deat
 	myEvent <- match.arg(event)
 	
 	# put doubling time in new column
-	dt_col <- paste(myEvent, "doubling_time", sep = "_")
+	dt_col <- paste(myEvent, "doubling_rate", sep = "_")
 	
 	dt_colLabel <- stringr::str_to_title(gsub("_", " ", dt_col))
 	
@@ -31,7 +31,8 @@ plotDoublingTime <- function(eData = getUsStateCases(), event = c("cases", "deat
 	sPlot <- ggplot2::ggplot(growthRateData, 
 													 ggplot2::aes_q(x = as.name("date"), y = as.name(dt_col))) +
 		ggplot2::geom_line(ggplot2::aes(group = state)) +
-		ggplot2::labs(x = "Date", y = "Doubling Time", title = dt_colLabel, 
+		ggplot2::labs(x = "Date", y = "Doublings per Day", 
+									title = dt_colLabel, 
 									subtitle = paste(myWindow, "Day Window"))
 	
 	if(!usePlotly) { return(sPlot) }
@@ -42,7 +43,7 @@ plotDoublingTime <- function(eData = getUsStateCases(), event = c("cases", "deat
 }
 
 
-# doublingRate: calculate doubling rate over window
+# doublingRate: calculate growth rate over window
 doublingRate <- function(myData, myWindow = 7){
 	zoo::rollapply(myData, myWindow, fit_series, fill = NA, align = "right")
 }
@@ -50,7 +51,7 @@ doublingRate <- function(myData, myWindow = 7){
 # fit_series: log transform and lmfit a series of numeric values
 
 fit_series <- function(vals) {
-	# calculate doubling time within window
+	# calculate grwoth rate within window
 	# make sure there's some change
 	if(diff(range(vals)) < 2) { return(NA)}
 	
@@ -73,5 +74,5 @@ fit_series <- function(vals) {
 	# since we're in base 2, the doubling time is 1/logslope
 	dtime <- 1/logslope
 	
-	return(unname(dtime))
+	return(unname(logslope))
 }
